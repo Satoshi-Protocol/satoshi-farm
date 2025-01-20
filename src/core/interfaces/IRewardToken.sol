e65746a57ac29e5331b9ea877db3b9d98358877a
abner.huang <fantasrulai@gmail.com>
2025-01-21 02:56:53 +0800

    update deploy

--- src/core/interfaces/IRewardToken.sol
+++ src/core/interfaces/IRewardToken.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.20;
 
 import { IOFT } from "../../layerzero/IOFT.sol";
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { IAccessControl } from "@openzeppelin/contracts/access/IAccessControl.sol";
 
-interface IRewardToken is IOFT, IERC20 {
+interface IRewardToken is IOFT, IERC20, IAccessControl {
     function mint(address to, uint256 amount) external;
 
     function burn(address from, uint256 amount) external;
