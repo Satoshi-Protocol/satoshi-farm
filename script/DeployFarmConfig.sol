1e4c47adac5bfc950416c2cf587fa27753fda946
Ray Huang <ray880301@gmail.com>
2025-01-27 04:10:38 +0800

    update script

--- script/DeployFarmConfig.sol
+++ script/DeployFarmConfig.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.20;
 
 import { FarmConfig } from "../src/core/interfaces/IFarm.sol";
 
-address constant FARM_MANAGER_ADDRESS = 0xEC4A8A4b9E09aE963D5FAbA4D3d8BA5D4C412b6B;
+address constant FARM_MANAGER_ADDRESS = 0x86B9c300d65CcC8Bd9284f5000D69df0EF0fb05E;
 address constant UNDERLYING_ASSET_ADDRESS = 0x59fdaB9956C5Dc85f2b9ceC31551cAb6f9C3897D;
 
 abstract contract DeployFarmConfig {
     FarmConfig internal FARM_CONFIG = FarmConfig({
-        depositCap: 0,
-        depositCapPerUser: 0,
-        depositStartTime: 0,
+        depositCap: type(uint256).max,
+        depositCapPerUser: type(uint256).max,
+        depositStartTime: uint32(block.timestamp),
         depositEndTime: type(uint32).max,
         rewardRate: 28_935_185_185_185_184,
         rewardStartTime: uint32(block.timestamp),
--- script/DeploySetupConfig.sol
+++ script/DeploySetupConfig.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.20;
 import { FarmConfig, IFarm } from "../src/core/interfaces/IFarm.sol";
 import { DstInfo, LzConfig } from "../src/core/interfaces/IFarmManager.sol";
 
-address constant REWARD_TOKEN_ADDRESS = 0x570AAe4E945Dff2F576f7aD2f529E982CD5C4D52;
+address constant REWARD_TOKEN_ADDRESS = 0x44621f077464a41849E2e3E972e07CBF6999c508;
 
 abstract contract DeploySetupConfig {
     FarmConfig internal REWARD_FARM_CONFIG = FarmConfig({
-        depositCap: 0,
-        depositCapPerUser: 0,
-        depositStartTime: 0,
+        depositCap: type(uint256).max,
+        depositCapPerUser: type(uint256).max,
+        depositStartTime: uint32(block.timestamp),
         depositEndTime: type(uint32).max,
         rewardRate: 5_208_333_333_333_333,
         rewardStartTime: uint32(block.timestamp),
@@ -24,12 +24,12 @@ abstract contract DeploySetupConfig {
 
     DstInfo internal DST_INFO = DstInfo({
         dstEid: 40_245,
-        dstRewardFarm: IFarm(address(0x32198912CF24A0240AE48F46FEDf92ECD3328EaC)),
-        dstFarmManagerBytes32: bytes32(uint256(uint160(0xa0CCAc865034E7a8f47297E130FCf117891cA155)))
+        dstRewardFarm: IFarm(address(0x589c06a481427E8Dfeb6a6066F9a9441e6Ac3965)),
+        dstFarmManagerBytes32: bytes32(uint256(uint160(0x43Ae83295a09117D82835302019159DfAAB6C640)))
     });
 
     LzConfig internal LZ_CONFIG = LzConfig({
-        eid: 40_161,
+        eid: 40161,
         endpoint: address(0x6EDCE65403992e310A62460808c4b910D972f10f),
         refundAddress: address(0xf13d1D93563c27b8C7ca4fCA1fcD9114d36139bb)
     });
