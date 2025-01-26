f51fd1df4e0c3902a4629008a8c7414a6505e2df
abner.huang <fantasrulai@gmail.com>
2025-01-27 03:21:21 +0800

    update

--- script/testnet/TestnetConfig.sol
+++ script/testnet/TestnetConfig.sol
@@ -28,7 +28,7 @@ library TestnetConfigHelper {
             depositCapPerUser: 500 ether,
             depositStartTime: uint32(block.timestamp),
             depositEndTime: uint32(block.timestamp + 30 days),
-            rewardRate: 10_000 * 10 ** 18,
+            rewardRate: 28935185185185184,
             rewardStartTime: uint32(block.timestamp),
             rewardEndTime: uint32(block.timestamp + 30 days),
             claimStartTime: uint32(block.timestamp),
@@ -45,7 +45,7 @@ library TestnetConfigHelper {
             depositCapPerUser: type(uint256).max,
             depositStartTime: uint32(block.timestamp),
             depositEndTime: uint32(block.timestamp + 30 days),
-            rewardRate: 10_000 * 10 ** 18,
+            rewardRate: 28935185185185184,
             rewardStartTime: uint32(block.timestamp),
             rewardEndTime: uint32(block.timestamp + 30 days),
             claimStartTime: uint32(block.timestamp),
@@ -62,7 +62,7 @@ library TestnetConfigHelper {
             depositCapPerUser: type(uint256).max,
             depositStartTime: uint32(block.timestamp),
             depositEndTime: uint32(block.timestamp + 30 days),
-            rewardRate: 10_000 * 10 ** 18,
+            rewardRate: 28935185185185184,
             rewardStartTime: uint32(block.timestamp),
             rewardEndTime: uint32(block.timestamp + 30 days),
             claimStartTime: uint32(block.timestamp),
@@ -79,7 +79,7 @@ library TestnetConfigHelper {
             depositCapPerUser: type(uint256).max,
             depositStartTime: uint32(block.timestamp),
             depositEndTime: uint32(block.timestamp + 30 days),
-            rewardRate: 10_000 * 10 ** 18,
+            rewardRate: 5208333333333333,
             rewardStartTime: uint32(block.timestamp),
             rewardEndTime: uint32(block.timestamp + 30 days),
             claimStartTime: uint32(block.timestamp),
@@ -93,11 +93,12 @@ library TestnetConfigHelper {
 
 abstract contract ArbSepTestnetConfig {
     address constant REWARD_TOKEN_ADDRESS = address(0x1e1d7C76Bd273d60E756322A8Ea9A1914327fa13);
+    string constant MEME_TOKEN_SYMBOL = "Ai16z";
 
     DstInfo DST_INFO = DstInfo({
-        dstEid: 40_231, // Arbitrum Sepolia chain
-        dstRewardFarm: IFarm(address(0xa140F9E1d45eb7107Fa9665BA975B3601007B61E)),
-        dstFarmManagerBytes32: bytes32(0x000000000000000000000000d7ee27a2f5a52643bb10c524211808163aff7bd3)
+        dstEid: 40332, // Arbitrum Sepolia chain
+        dstRewardFarm: IFarm(address(0xB90333dDF932D7B1Ea3ea4F4f5C47E5B1ed00213)),
+        dstFarmManagerBytes32: bytes32(0x00000000000000000000000029a2e39aee8941e5d59568e9a981c3b21ffce1ee)
     });
 
     LzConfig LZ_CONFIG = LzConfig({
@@ -109,16 +110,41 @@ abstract contract ArbSepTestnetConfig {
 
 abstract contract BaseSepTestnetConfig {
     address constant REWARD_TOKEN_ADDRESS = address(0x819591a4e747212EDA0880DD2F171B582Ce4149B);
+    string constant MEME_TOKEN_SYMBOL = "Ai16z";
 
     DstInfo DST_INFO = DstInfo({
-        dstEid: 40_231, // Arbitrum Sepolia chain
-        dstRewardFarm: IFarm(address(0xa140F9E1d45eb7107Fa9665BA975B3601007B61E)),
-        dstFarmManagerBytes32: bytes32(0x000000000000000000000000d7ee27a2f5a52643bb10c524211808163aff7bd3)
+        dstEid: 40332, // Arbitrum Sepolia chain
+        dstRewardFarm: IFarm(address(0xB90333dDF932D7B1Ea3ea4F4f5C47E5B1ed00213)),
+        dstFarmManagerBytes32: bytes32(0x00000000000000000000000029a2e39aee8941e5d59568e9a981c3b21ffce1ee)
     });
 
     LzConfig LZ_CONFIG = LzConfig({
         eid: 40_245, // BASE Sepolia chain
         endpoint: address(0x6EDCE65403992e310A62460808c4b910D972f10f),
+        refundAddress: 0xb031931f4A6AB97302F2b931bfCf5C81A505E4c2
+    });
+}
+
+
+abstract contract HyperliquidTestnetConfig {
+    address constant REWARD_TOKEN_ADDRESS = address(0x46Ff6484BeB9B4e368eED4B5bBc5609BE44415eF);
+    string constant MEME_TOKEN_SYMBOL = "HYPE";
+
+    DstInfo DST_INFO = DstInfo({
+        dstEid: 40332, // Arbitrum Sepolia chain
+        dstRewardFarm: IFarm(address(0)),
+        dstFarmManagerBytes32: bytes32(0)
+    });
+
+    // DstInfo DST_INFO = DstInfo({
+    //     dstEid: 40_231, // Arbitrum Sepolia chain
+    //     dstRewardFarm: IFarm(address(0x922C00923534415203472Da91F8BbEA5782674E8)),
+    //     dstFarmManagerBytes32: bytes32(0x000000000000000000000000b4bb342294fe7d0d2ebdd894498b27bba13d5f1b)
+    // });
+
+    LzConfig LZ_CONFIG = LzConfig({
+        eid: 40332, // BASE Sepolia chain
+        endpoint: address(0x6Ac7bdc07A0583A362F1497252872AE6c0A5F5B8),
         refundAddress: 0xb031931f4A6AB97302F2b931bfCf5C81A505E4c2
     });
 }
