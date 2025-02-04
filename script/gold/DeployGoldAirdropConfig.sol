dadced650023bb2424c1d6d04581cdad61568857
Ray Huang <ray880301@gmail.com>
2025-02-04 23:55:07 +0800

    add deploy

--- script/gold/DeployGoldAirdropConfig.sol
+++ script/gold/DeployGoldAirdropConfig.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-address constant GOLD_ADDRESS = 0x17b663455c5D57f10f08636c323F5991fFe91156;
+address constant GOLD_ADDRESS = 0x2ffB8C5ecA944c1f34c60e597Ebed15aB0a470d4;
 uint256 constant START_TIME = 1_738_728_000; // Wed Feb 05 2025 12:00:00 GMT+0800
 uint256 constant END_TIME = 1_741_147_200; // Wed Mar 05 2025 12:00:00 GMT+0800
 bytes32 constant MERKLE_ROOT = 0x0;
